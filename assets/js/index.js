a89c482a19b66763ec30c167eda623de469e975d
Gabriel Belentani Felipe <gabriel.portifolio07@gmail.com>
2025-01-29 13:57:45 -0300

    MODIFY: comentando o código da pagina inicial

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,14 @@
+// criando a função chamada pelo botão de acessar
 function acessar(){
+    // criando uma constante para armazenar o valor do nome digitado
     const nome = document.getElementById('nome').value;
+    // verificando se o nome foi digitado
     if(nome == ''){
+        // notificando o usuário no caso do nome ser inválido
         alert('Nome não detectado, digite um nome válido!');
+        // se não cair na verificação
     }else{
+        // manda o valor do nome para o documento principal
         localStorage.setItem('nome', nome);
         window.location.href = '../../templates/home.html'
     }
