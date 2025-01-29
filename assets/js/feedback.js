2368e972cb79d39d613afef7bf4f89f780596b8d
Gabriel Belentani Felipe <gabriel.portifolio07@gmail.com>
2025-01-29 13:52:16 -0300

    ADD: comentando código

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,8 +1,16 @@
-let button = document.getElementById('no');
+
+
+
+// criando uma variável que armazena o botão não
+let button = document.getElementById('nao');
+//criando variavel par armazenar a altura da página
 let height = window.innerHeight - 50;
+//criando variavel par armazenar a largura da página
 let width = window.innerWidth - 50;
 
+// atribuindo um evento ao bottão
 button.addEventListener('mouseover', function(){
+    // randomizando a posição x e y
     button.style.position = 'absolute';
     button.style.top = Math.random() * height + 'px';
     button.style.left = Math.random() * width + 'px';
